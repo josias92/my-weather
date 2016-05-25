2a1f01b45c12ecab020bbe38a40df92be650f00c
josias92 <jnavarr2@engr-131-13.eastern.ewu.edu>
2016-05-25 16:36:54 -0700

    Geolocation + Forecast

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,41 @@ $.simpleWeather({
       // Display Data
       $('#cheney .city').text(weather.city);
       $('#cheney .temp').text(weather.temp);
+      // Display icon
+      $('#cheney i').addClass( 'icon-' + weather.code );
+      // Get forecast
+      $('#d1').text(weather.forecast[1].date);
+      $('#d1').text(weather.forecast[1].high);
+      $('#d1').text(weather.forecast[1].code);
+        
+        // Add custom body class
+        if (weather.code >= 26 && weather.code <= 30) {
+            //remove class
+            $('body').removeClass();
+            //add class
+            $('body').addClass('bg-1');
+            
+        }
+        
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    }
+  
+  });
+
+// Get Spokane Weather
+$.simpleWeather({
+    location: '99004',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('#cheney .city').text(weather.city);
+      $('#cheney .temp').text(weather.temp);
       $('#cheney i').addClass( 'icon-' + weather.code );
         
       // Entire weather object
@@ -40,6 +75,55 @@ $.simpleWeather({
   
   });
 
+// GEOLOCATION
+
+// Get Geolocation
+if ('geolocation' in navigator) {
+
+$('.geo button').show(); 
+
+} else {
+
+$('.geo button').hide();
+$('.geo').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+// On Click, Get Geolocation, Call Weather Function
+$('.geo .button').click( function() {
+  
+     
+    //load weather using your lat/lng coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+        getWeather(position.coords.latitude+','+position.coords.longitude); 
+    });
+   
+});
+
+// Get Geolocated Weather
+var getWeather = function(location) {
+
+    $.simpleWeather({
+    location: location,
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.geo .temp').text(weather.temp);
+      $('.geo .city').text(weather.city);
+     // display icon
+      $('.geo i').addClass('icon-' + weather.code);
+    
+      // Entire weather object
+      console.log();
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    }
 
 
+};
+                    
 
+                    
